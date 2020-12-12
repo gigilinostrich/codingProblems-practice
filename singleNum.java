a398a4e41d48b6582f652d6a45b6030bd0bc0914
gigilinostrich <74795941+gigilinostrich@users.noreply.github.com>
2020-12-12 00:23:33 -0600

    Add files via upload

---
+++ singleNum.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class singleNum {
+
+	public static void main(String[] args) {
+	
+		int[] nums = { 4, 1, 2, 1, 2 };
+		// key: array element value: element counts
+		HashMap<Integer, Integer> elementCountMap = new HashMap<Integer, Integer>();
+
+		for (int i : nums) { // iterating through the num array
+			System.out.println("i " + i); // printing i means printing array elements
+			Integer count = elementCountMap.get(i);
+			System.out.println("count is " + count); // currently the key is null in the map
+			// If element is present in elementCountMap, incrementing its count by 1
+			// if one specific array element is in the map, it means there are duplicates.
+			// Increase count (value)
+			if (elementCountMap.containsKey(i)) {
+				elementCountMap.put(i, count + 1);
+			} else { // if one specific key (element in array ) does not exist, add 1 as initial
+						// count
+						// If element is not present in elementCountMap,
+						// adding this element to elementCountMap with 1 as it's value
+				elementCountMap.put(i, 1); // start with (key: any elements in array | Value: 1) in map
+			}
+
+		}
+		System.out.println("inside the elementCountMap " + elementCountMap);
+		for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+			if (entry.getValue() == 1) {
+				System.out.println(entry.getKey());
+				
+			}
+		}
+
+	}
+}
