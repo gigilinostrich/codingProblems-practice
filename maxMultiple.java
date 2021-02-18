635ce792b9f4b09a7c77f93b27c9a18877b06874
gigilinostrich <74795941+gigilinostrich@users.noreply.github.com>
2021-02-18 00:14:50 -0600

    Add files via upload

---
+++ maxMultiple.java
@@ -0,0 +1,30 @@
+
+public class maxMultiple {
+	/*
+	 * Given a divisor and a bound, find the largest integer N such that:
+	 * 
+	 * N is divisible by divisor. N is less than or equal to bound. N is greater
+	 * than 0.
+	 * 
+	 * It is guaranteed that such a number exists.
+	 * 
+	 * Example
+	 * 
+	 * For divisor = 3 and bound = 10, the output should be maxMultiple(divisor,
+	 * bound) = 9.
+	 * 
+	 * The largest integer divisible by 3 and not larger than 10 is 9.
+	 */
+	public static void main(String[] args) {
+		int divisor = 3;
+		int bound = 10;
+		int max = Integer.MIN_VALUE;
+		for (int i = 1; i <= bound; i++) {
+			if (i % divisor == 0) {
+				max = Math.max(max, i);
+			}
+		}
+		System.out.println(max);
+	}
+
+}
